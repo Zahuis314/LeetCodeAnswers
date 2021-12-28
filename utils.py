8e2f293364571404f734d6fb1ad993f95033bf4b
Roberto Zahuis <zahuis314@gmail.com>
2021-12-28 02:04:19 -0500

    Add python utility

---
+++ utils.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+    def __str__(self):
+        return str(self.__repr__())
+
+    def __repr__(self):
+        current = [self.val]
+        if self.next:
+            current.extend(self.next.__repr__())
+        return current
+
+def convert_to_list_node(arr):
+    if len(arr) == 1:
+        return ListNode(arr[0])
+    return ListNode(arr[0],convert_to_list_node(arr[1:]))
