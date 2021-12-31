309df2a420d3bb2c7b1c2c19aeb2ee071cb46d0a
Roberto Zahuis <zahuis314@gmail.com>
2021-12-31 00:02:51 -0500

    Update utils.rb

--- utils.rb
+++ utils.rb
@@ -1,11 +1,12 @@
 # Definition for Node.
 class Node
     attr_accessor :val, :left, :right, :next
-    def initialize(val)
+    def initialize(val, left = nil, right = nil)
         @val = val
-        @left, @right, @next = nil, nil, nil
+        @left, @right, @next = left, right, nil
     end
     def to_s()
-        return @val.to_s
+        # return @val.to_s
+        return [@val,@next.val].to_s
     end
 end
