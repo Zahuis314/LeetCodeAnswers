1b8c52faa0c64b6592f100072b7c40c676ea95c4
Roberto Zahuis <zahuis314@gmail.com>
2022-01-08 03:34:13 -0500

    Add 46. Permutations.rb

---
+++ 46. Permutations.rb
@@ -0,0 +1,12 @@
+# Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+# 1 <= nums.length <= 6
+# -10 <= nums[i] <= 10
+# All the integers of nums are unique.
+
+
+# @param {Integer[]} nums
+# @return {Integer[][]}
+def permute(nums)
+    nums.permutation.to_a
+end
